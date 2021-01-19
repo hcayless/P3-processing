ea56e345b134b94c97d73585d2b88658afb401bd
Hugh Cayless <philomousos@gmail.com>
2021-01-19 11:34:00 -0500

    Adding GitHub Action.

---
+++ .github/actions/process-articles/process.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILES=$(echo $CHANGED_FILES | grep "sources/*.docx")
+
+for f in $FILES
+do
+  OUT = $(echo $f | sed 's/\/sources\//\/articles\//' | sed 's/.docx/.xml/')
+  saxon -s:"$f" -xsl:xslt/process-tei.xsl -o:"$OUT-1"
+  bin/process-leiden.sh epidoc
+  saxon -s:"$OUT-1" -xsl:xslt/process-tei-2.xsl -o:"$OUT"
+  rm "$OUT-1"
+done
