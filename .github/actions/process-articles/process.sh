a09d2f84b6cdac74f9a537f9e917dbcbd63ae5c2
Hugh Cayless <philomousos@gmail.com>
2021-01-19 13:08:42 -0500

    Updating action script.

--- .github/actions/process-articles/process.sh
+++ .github/actions/process-articles/process.sh
@@ -5,8 +5,9 @@ FILES=$(echo $CHANGED_FILES | grep "sources/*.docx")
 for f in $FILES
 do
   OUT = $(echo $f | sed 's/\/sources\//\/articles\//' | sed 's/.docx/.xml/')
-  saxon -s:"$f" -xsl:xslt/process-tei.xsl -o:"$OUT-1"
+  docxtotei "$f" "$OUT-1"
+  saxon -s:"$OUT-1" -xsl:xslt/process-tei.xsl -o:"$OUT-2"
   bin/process-leiden.sh sources/epidoc
-  saxon -s:"$OUT-1" -xsl:xslt/process-tei-2.xsl -o:"$OUT"
-  rm "$OUT-1"
+  saxon -s:"$OUT-2" -xsl:xslt/process-tei-2.xsl -o:"$OUT"
+  rm "$OUT-1" "$OUT-2"
 done
