fa513677b69e583a619379a7a78bab38e0e06be9
Hugh Cayless <philomousos@gmail.com>
2021-01-19 13:39:23 -0500

    Adding troubleshooting output.

--- .github/actions/process-articles/process.sh
+++ .github/actions/process-articles/process.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 FILES=$(echo $CHANGED_FILES | grep "sources/*.docx")
+echo "Processing...\n$FILES"
 
 for f in $FILES
 do
   OUT = $(echo $f | sed 's/\/sources\//\/articles\//' | sed 's/.docx/.xml/')
+  echo "Converting $f to $OUT-1" 
   docxtotei "$f" "$OUT-1"
+  echo "Converting $OUT-1 to $OUT-2"
   saxon -s:"$OUT-1" -xsl:xslt/process-tei.xsl -o:"$OUT-2"
-  bin/process-leiden.sh sources/epidoc
+  echo "Processing Leiden"
+  bin/process-leiden.sh xslt/epidoc
+  echo "Converting $OUT-2 to $OUT"
   saxon -s:"$OUT-2" -xsl:xslt/process-tei-2.xsl -o:"$OUT"
   rm "$OUT-1" "$OUT-2"
 done
