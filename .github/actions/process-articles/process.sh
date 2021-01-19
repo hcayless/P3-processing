28d16ec51f3e3dfd84d69f8dd7d80529d4f0a000
Hugh Cayless <philomousos@gmail.com>
2021-01-19 11:41:35 -0500

    Bugfixes

--- .github/actions/process-articles/process.sh
+++ .github/actions/process-articles/process.sh
@@ -6,7 +6,7 @@ for f in $FILES
 do
   OUT = $(echo $f | sed 's/\/sources\//\/articles\//' | sed 's/.docx/.xml/')
   saxon -s:"$f" -xsl:xslt/process-tei.xsl -o:"$OUT-1"
-  bin/process-leiden.sh epidoc
+  bin/process-leiden.sh sources/epidoc
   saxon -s:"$OUT-1" -xsl:xslt/process-tei-2.xsl -o:"$OUT"
   rm "$OUT-1"
 done
