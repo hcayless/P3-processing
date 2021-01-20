a87bffdfa0a94e2516ae1dfa377f5235d3101302
Hugh Cayless <philomousos@gmail.com>
2021-01-20 12:08:37 -0500

    More troubleshooting output.

--- bin/process-leiden.sh
+++ bin/process-leiden.sh
@@ -4,9 +4,12 @@ if [ -f "$1" ]
 then
   for f in `ls $1/*.lplus` 
   do
+  echo "Converting $f"
   curl --data-urlencode content@$f \
     -d 'type=epidoc' -d 'direction=nonxml2xml' \
-    http://libdc3-dev-03.oit.duke.edu/xsugar/ \
+    https://libdc3-dev-03.oit.duke.edu/xsugar/ \
     | jq -r .content > $(echo $f | sed 's/.lplus/.xml/')
   done
+else
+  echo "No Leiden+ files found in $1"
 fi
