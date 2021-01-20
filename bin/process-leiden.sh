ff7484a2997657a6d9eaacb8c4d5828a7145489c
Hugh Cayless <philomousos@gmail.com>
2021-01-20 11:17:22 -0500

    Only run Leiden+ processing if there is any.

--- bin/process-leiden.sh
+++ bin/process-leiden.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-for f in `ls $1/*.lplus` 
-do
-curl --data-urlencode content@$f \
-  -d 'type=epidoc' -d 'direction=nonxml2xml' \
-  http://libdc3-dev-03.oit.duke.edu/xsugar/ \
-  | jq -r .content > $(echo $f | sed 's/.lplus/.xml/')
-done
+if [ -f "$1" ]
+then
+  for f in `ls $1/*.lplus` 
+  do
+  curl --data-urlencode content@$f \
+    -d 'type=epidoc' -d 'direction=nonxml2xml' \
+    http://libdc3-dev-03.oit.duke.edu/xsugar/ \
+    | jq -r .content > $(echo $f | sed 's/.lplus/.xml/')
+  done
+fi
