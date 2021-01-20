d7b23086ae8d376f335e71ff47679a636e1ddd51
Hugh Cayless <philomousos@gmail.com>
2021-01-20 13:19:10 -0500

    Use hcayless_P3 branch of Stylesheets.

--- .github/actions/process-articles/process.sh
+++ .github/actions/process-articles/process.sh
@@ -10,7 +10,7 @@ for f in $FILES
 do
   OUT=$(echo $f | sed 's/sources\//articles\//' | sed 's/.docx/.xml/')
   echo "Converting $f to $OUT-1" 
-  docxtotei "$f" "$OUT-1"
+  /opt/actions/Stylesheets/bin/docxtotei "$f" "$OUT-1"
   echo "Converting $OUT-1 to $OUT-2"
   saxon -s:"$OUT-1" -xsl:xslt/process-tei.xsl -o:"$OUT-2"
   echo "Processing Leiden"
